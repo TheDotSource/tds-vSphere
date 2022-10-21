09f3200ee8166b867c036052c1bb41a6ba1225ae
Alistair M <49152002+TheDotSource@users.noreply.github.com>
2022-10-21 07:56:33 +0100

    Accommodations for ESXi 8.0
    ESXi 8.0 no longer supports allowLegacyCPU. Added alternative method for deprecated CPUs.

--- Public/New-EsxiAutoIso.ps1
+++ Public/New-EsxiAutoIso.ps1
@@ -42,6 +42,9 @@ function New-EsxiAutoIso {
         Optional. Useful for nested virtualisation. VMK0 inherits a MAC address from the physical adapter, in a nested scenario this can cause issues.
         Use this switch to recreate VMK0 with a fresh MAC.
 
+    .PARAMETER allowUnsupportedCPU
+        Perform the installation even if the host CPU is not supported.
+
    .INPUTS
         None.
 
@@ -81,7 +84,9 @@ function New-EsxiAutoIso {
         [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
         [string]$hostname,
         [Parameter(Mandatory=$false,ValueFromPipeline=$false)]
-        [switch]$resetVmk0
+        [switch]$resetVmk0,
+        [Parameter(Mandatory=$false,ValueFromPipeline=$false)]
+        [switch]$allowLegacyCPU
     )
 
 
@@ -100,11 +105,13 @@ function New-EsxiAutoIso {
 
         ## Attempt to get disc info from media path
         try {
-            $esxVersion = Get-Content -Path ($mediaPath + "\.DISCINFO") -ErrorAction Stop
-            Write-Verbose ("Media version is " + $esxVersion[1].split(": ")[1])
+            [xml]$esxXml = Get-Content -Path ($mediaPath + "\UPGRADE\METADATA.XML") -ErrorAction Stop
+            $esxVersion = [system.version]$esxXml.vum.product.esxVersion
+
+            Write-Verbose ("Media version is " + $esxVersion.ToString())
         } # try
         catch {
-            Write-Warning ("Failed to open ESXi .DISCINFO descriptor. Verify valid media has been extracted to the path specified.")
+            Write-Warning ("Failed to open ESXi \UPGRADE\METADATA.XML. Verify valid media has been extracted to the path specified.")
         } # catch
 
 
@@ -133,18 +140,33 @@ function New-EsxiAutoIso {
         ## Configure start of template
         Write-Verbose ("Processing ks.cfg with specified values.")
 
+        ## If we are using allowLegacyCPU this needs some special parameters for ESXi 8.0 or above.
+        ## ESXi 8.0 no longer appears to use the allowLegacyCPU boot option but relies on some install parameters
+        if (($esxVersion.Major -ge 8) -and ($allowLegacyCPU)) {
+            Write-Verbose ("Applying install parameters for ESXi 8.0 and above for unsupported CPU type.")
+            $installString = "install --firstdisk --overwritevmfs --ignoreprereqwarnings --ignoreprereqerrors --forceunsupportedinstall"
+        } # if
+        else {
+            ## Standard install string, previous versions work with allowLegacyCPU boot option
+            $installString = "install --firstdisk --overwritevmfs"
+        } # else
+
+
         $ksTemplate = @"
 ### Accept the VMware End User License Agreement
 vmaccepteula
 
-### Set the root password for the DCUI and Tech Support Mode
-rootpw {0}
-
 ### The install media (priority: local / remote / USB)
-install --firstdisk --overwritevmfs
+{0}
 
 ### Reboot ESXi Host
 reboot
+
+### Set the network to  on the first network adapter
+network --bootproto=static --device=vmnic0 --ip={1} --netmask={2} --gateway={3} --nameserver={4} --hostname={5} --addvmportgroup=0
+
+### Set the root password for the DCUI and Tech Support Mode
+rootpw {6}
 
 
 #Firstboot section 1
@@ -160,31 +182,26 @@ esxcli network vswitch standard portgroup add -p "VM Network" -v vSwitch0
         if ($resetVmk0) {
 
             Write-Verbose ("Adding ESXCLI for VMK0 reset.")
-            
+
             $ksTemplate += @"
 
 ### Reset VMK0. By default, VMK0 inherits a MAC from the physical adapter which can cause anomalies with nested networking.
 esxcli network ip interface remove --interface-name=vmk0
 esxcli network ip interface add --interface-name=vmk0 --portgroup-name="Management Network"
-esxcli network ip interface ipv4 set -i vmk0 -I {1} -N {2} -t static
-
+esxcli network ip interface ipv4 set -i vmk0 --ipv4 {1} --netmask {2} --type static
 "@
         } # if
 
         ## Append network config as the last thing we do
         $ksTemplate += @"
-        
+
 ### Suppress Core Dump Warning
 esxcli system settings advanced set -o /UserVars/SuppressCoredumpWarning -i 1
-
-
-### Set the network to  on the first network adapter
-network --bootproto=static --device=vmnic0 --ip={1} --netmask={2} --gateway={3} --nameserver={4} --hostname={5} --addvmportgroup=0"
 "@
 
         ## Inject template values
         try {
-            $ksTemplate = $ksTemplate -f $rootCredential.GetNetworkCredential().Password, $ip, $netmask, $gateway, $nameserver, $hostname
+            $ksTemplate = $ksTemplate -f $installString, $ip, $netmask, $gateway, $nameserver, $hostname, $rootCredential.GetNetworkCredential().Password
             Write-Verbose ("Template values injected.")
         } # try
         catch {
